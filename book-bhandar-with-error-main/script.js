82bdd3fc7fd5a5eb13e6fd96bb5fd5d47f3e0d7d
tanianasrin1 <tasrineva169@gmail.com>
2022-03-10 10:03:41 +0600

    bug fix

--- book-bhandar-with-error-main/script.js
+++ book-bhandar-with-error-main/script.js
@@ -88,11 +88,17 @@ const showBooks = (books) => {
 };
 
 const createCard = (book) => {
+  console.log(book)
   const div = document.createElement("div");
   div.classList.add("card");
 
-  let overview = book.overview;
-
+  let overview 
+  if(book.overview.length > 100){
+   overview = book.overview.slice(0,100) + "..."
+  }
+  else{
+   overview=book.overview
+  }
   div.innerHTML = `
   <div class="image-container">
     <img
